7fd50c8488f411aa78a60bd6eb7f2d20ce95cef4
Dalgomz <33590258+Dalgomz@users.noreply.github.com>
2018-08-26 19:45:34 -0500

    Actualizada la vista del código

--- RobotProlog/release.pl
+++ RobotProlog/release.pl
@@ -2,7 +2,9 @@
 % Usar go([estado inicial],[estado final])
 % Los estados tienen la forma [Robot,CajaAzul,CajaVerde,Pinza]
 % Ej: go([h1,h1,h1,v],[h2,h1,v,cv]).
- % Funciones cuyo funcionamiento y razon de existir se deben confiar a seres trascendidos
+
+
+% Funciones cuyo funcionamiento y razon de existir se deben confiar a seres trascendidos
 empty_set([]).  %Set vacio
 empty_sort_queue([]). %Cola vacia
 insert_sort_queue(State, [], [State]). %Insertar a la cola ordenada (1 ite)
@@ -13,14 +15,18 @@ add_to_set(X, S, [X|S]).
 add_to_set(X, S, S) :- member(X, S), !.  %Agrega al set, al ser member positivo da True
 member_sort_queue(E, S) :- member(E, S). %Comprueba si es miembro de la cola ordenada
 member_set(E, S) :- member(E, S). %Comprueba si es miembro del set
- % Guarda los valores de los estados en forma de una lista
+
+
+% Guarda los valores de los estados en forma de una lista
 % G: Posición
 % H: Valor Heuristico
 % F: F
 % Costo: Valor de función de costo
 % Lista: Siguiente estado
 state_record(State, Parent, G, H, F, Costo, [State, Parent, G, H, Costo, F]).
- % Inicia Start estado inicial, Goal estado final.
+ 
+ 
+% Inicia Start estado inicial, Goal estado final.
 % Los caminos estan dados por path
 % Open cola los estados abiertos (Posibles), Closed los estados cerrados
 go(Start, Goal) :-
@@ -30,11 +36,13 @@ go(Start, Goal) :-
      state_record(Start, nil, 0, H, H, 0, First_record),
     insert_sort_queue(First_record, Empty_open, Open),
     path(Open,Closed, Goal).
+ 
  %Cola ordenada vacia
 path(Open_pq, _,_) :-
        empty_sort_queue(Open_pq),
        write('No hay solucion.').
- %Siguiente ultimo estado, final
+
+%Siguiente ultimo estado, final
 %Escribe los estados en orden
 path(Open, Closed, Goal) :-
     %saca el primer elemento
@@ -43,7 +51,8 @@ path(Open, Closed, Goal) :-
     State = Goal,
     write('El camino de la solución es: '), nl,
     printsolution(First_record, Closed).
- % Siguiente no es ultimo 
+ 
+% Siguiente no es ultimo 
 % Genera sus hijos, agrega a abiertos y continua
 path(Open, Closed, Goal) :-
     %saca el primer elemento de la lista
@@ -53,7 +62,8 @@ path(Open, Closed, Goal) :-
     insert_list(Children, Rest_of_open, New_open),
     add_to_set(First_record, Closed, New_closed),
     path(New_open, New_closed, Goal),!.
- % Genera todos los hijos a los que no se ha pasado a abierto o cerrado y se unen dependiendo de sus valores heuristicos
+ 
+% Genera todos los hijos a los que no se ha pasado a abierto o cerrado y se unen dependiendo de sus valores heuristicos
 moves(State_record, Open, Closed,Child, Goal) :-
     state_record(State, _, G, _, _, Costo, State_record),
     	opcion(State, Next, CostoOpcion),
@@ -66,43 +76,46 @@ moves(State_record, Open, Closed,Child, Goal) :-
     heuristica(Next, Goal, H,_,_,_,_),
     F is G_new + H,
     state_record(Next, State, G_new, H, F, NewCosto, Child).
- % Funciones de Coger
+ 
+
+% Funciones de Coger
 opcion(State,Next,CostoOpcion):- 
-    
     s(C1,State,L1), 
     s(C2,L1,L2),
     s(C3,L2,L3),
     s(C4,L3,_), C4 == v, C1 == C2,
     CostoOpcion is 2,
     Next = [C1,v,C3,ca].
- opcion(State,Next,CostoOpcion):- 
     
+ opcion(State,Next,CostoOpcion):- 
     s(C1,State,L1),
     s(C2,L1,L2),
     s(C3,L2,L3),
     s(C4,L3,_), C4 == v, C1 == C3,
     CostoOpcion is 2,
     Next = [C1,C2,v,cv].
- % Funciones de Movimiento 
+
+
+% Funciones de Movimiento 
 opcion(State,Next,CostoOpcion):- 
-    
     s(C1,State,L1),
     s(C2,L1,L2), 
     s(C3,L2,L3), 
     s(C4,L3,_), C1 == h1,
     CostoOpcion is 1,
     Next = [h2,C2,C3,C4].
- opcion(State,Next,CostoOpcion):- 
     
+ opcion(State,Next,CostoOpcion):- 
     s(C1,State,L1), 
     s(C2,L1,L2),
     s(C3,L2,L3),
     s(C4,L3,_), C1 == h2,
     CostoOpcion is 1,
     Next = [h1,C2,C3,C4].
- % Funciones de Soltar
-opcion(State,Next,CostoOpcion):-
     
+    
+% Funciones de Soltar
+opcion(State,Next,CostoOpcion):-
     s(C1,State,L1), 
     s(_,L1,L2),
     s(C3,L2,L3),
@@ -111,27 +124,33 @@ opcion(State,Next,CostoOpcion):-
     Next = [C1,C1,C3,v].
     
 opcion(State,Next,CostoOpcion):-
-    
     s(C1,State,L1), 
     s(C2,L1,L2),
     s(_,L2,L3),
     s(C4,L3,_), C4 \== v,
     CostoOpcion is 4,
     Next = [C1,C2,C1,v].
- % Funcion que sirve para recorrer listas en las opciones
+    
+% Funcion que sirve para recorrer listas en las opciones
 s(Cabeza,[Cabeza|LoDemas],LoDemas).
- % Inserta en la lista de resultados y la cola ordenada
+
+
+% Inserta en la lista de resultados y la cola ordenada
 insert_list([], L, L).
 insert_list([State | Tail], L, New_L) :-
     insert_sort_queue(State, L, L2),
     insert_list(Tail, L2, New_L).
- % Inicio de funcion heuristica
+    
+    
+% Inicio de funcion heuristica
 costoRobot([X,_,_,_],[A,_,_,_], Difer):-((X\==A) , Difer is 1) ; (X==A), Difer is 0.
 costoAzul([_,X,_,_],[_,A,_,_], Difer):-((X\==A) , Difer is 1) ; (X==A), Difer is 0.
 costoVerde([_,_,X,_],[_,_,A,_], Difer):-((X\==A) , Difer is 1) ; (X==A), Difer is 0.
 costoPinza([_,_,_,X],[_,_,_,A], Difer):-((X\==A) , Difer is 1) ; (X==A), Difer is 0.
  heuristica(Nodo, Fin, Costo, A,B,C,D):- costoRobot(Nodo, Fin, A), costoAzul(Nodo, Fin, B),costoVerde(Nodo, Fin, C),costoPinza(Nodo, Fin, D) , Costo is A+B+C+D.
 % Fin de funcion heuristica
+
+
  % Predicados para impresion de solucion
 % Primero en la lista
 printsolution(Next_record, _):-
