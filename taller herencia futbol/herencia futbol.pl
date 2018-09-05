db1d21caa94a499e941338f63502675427845e44
Daniel Hamilton Smith <danihas@live.com>
2018-09-05 17:55:19 -0500

    intentos... intentos

--- taller herencia futbol/herencia futbol.pl
+++ taller herencia futbol/herencia futbol.pl
@@ -7,30 +7,44 @@ contiene(persona, derecho).
 
 es(hombreAdulto,persona).
 contiene(hombreAdulto,1.80).
-
+cAltura(hombreAdulto,Altura):- contiene(hombreAdulto,Altura,_),!.
 
 es(jugadorFutbol,hombreAdulto).
 contiene(jugadorFutbol,1.85,3).
-
-
+cAltura(jugadorFutbol,Altura):- contiene(jugadorFutbol,Altura,_),!.
+cGoles(jugadorFutbol,Gol):- contiene(jugadorFutbol,_,Gol),!.
 
 es(defensa,jugadorFutbol).
 contiene(defensa,1).
+cGoles(defensa,Gol):- contiene(defensa,Gol),!.
 
 es(delantero,jugadorFutbol).
 contiene(delantero,3).
+cGoles(delantero,Gol):- contiene(delantero,Gol),!.
 
 
 eq(edith,millonarios).
 eq(miguel,santafe).
 
 
+es(edith,defensa).
+es(miguel,delantero).
+
+/*
+cual(N,Pos):- es(N,Pos).
+cual(N,Pos):- es(N,Pos2),cual(Pos2,Pos).
 
 
-inst(edith,defensa).
+goles(Nombre,Ngoles):-
+    cual(Nombre,P),cGoles(P,Ngoles).
 
-inst(miguel,delantero).
-
+altura(Nombre,Altura):-
+    cual(Nombre,P),cAltura(P,Altura).
+equipo(Nombre,Equipo):-
+    %inst(Nombre,Pos),
+    eq(Nombre,Equipo).
+*/
+/*este ya esta completamente funcional
 goles(Nombre,Ngoles):-
     inst(Nombre,Pos),contiene(Pos,Ngoles).
 altura(Nombre,Altura):-
@@ -38,3 +52,6 @@ altura(Nombre,Altura):-
 equipo(Nombre,Equipo):-
     %inst(Nombre,Pos),
     eq(Nombre,Equipo).
+*/
+
+
