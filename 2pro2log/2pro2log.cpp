426354ac5addfab9efdebab4fb315493072c6a55
Daniel Hamilton Smith <danihas@live.com>
2018-09-09 18:10:59 -0500

    dandole mas forma

--- 2pro2log/2pro2log.cpp
+++ 2pro2log/2pro2log.cpp
@@ -9,41 +9,23 @@ using namespace std;
 
 class Tipo{
 	
-	string nom;
+	public:
+		
+	string nombre;
+	Tipo *significado; // solo apunta en caso de coenzar con mayuscula nombre
 	
-	
-	/*bool comparar(Tipo cc)
+	Tipo(string nom)
 	{
-		int valEquiv  = 0; // cantidad de valores iguales
-		bool ret = false;
-		
-		if(this.nom == cc.nom && this.ndatos == cc.ndatos)
-		{
-			
-			
-			for (int a = 0 ; a < this.ndatos ; a ++)
-			{
-				if(this.valores[a].comparar(cc.valores[a])
-				{
-					valEquiv ++ ; 
-				}
-			}
-			
-		}
-		
-		
-		
-		
-	}*/
-	
+		this->nombre = nom;
+	}
 };
 class Axioma{
 	
 	//private:
-	
+	public:
 	string nombre;
 	int ndatos;
-	vector<string> valores; 
+	vector<Tipo> valores; 
 	
 	// Ej: del axioma conquistador(marco,roma)  nombre = conquistador  ,  valores = [marco,roma] con ndatos = 2 (el tamaño de la lista valores)
 	
@@ -53,7 +35,7 @@ class Axioma{
 	
 	public:
 	
-	Axioma( string nom, vector<string> val)
+	Axioma( string nom, vector<Tipo> val)
 	{
 		this->nombre  = nom;
 				
@@ -70,46 +52,62 @@ class Axioma{
 	{
 		
 		cout<<this->nombre<<" : ";
-		vector<string>::iterator it;// = this->valores.begin();
+		vector<Tipo>::iterator it;// = this->valores.begin();
 		
 		for(it = this->valores.begin(); it != this->valores.end(); it++ )    
 		{
   		  
-        	cout<< *it << " ";  // prints d.
+        	cout<<  it->nombre << " ";  // prints d.
         		
 		}
 		cout<<endl;
 	}
-	/*void comparar(Axioma cc)
-	{
-		//int nvalEquiv = 0; // cantidad de valores no nulos 
-		int valEquiv  = 0; // cantidad de valores iguales
-		
-		if(this.nombre == cc.nombre && this.ndatos == cc.ndatos)
-		{
-			
-			
-			for (int a = 0 ; a < this.ndatos ; a ++)
-			{
-				int equiv = this.valores[a].comparar(cc.valores[a])
-				
-				
-				if( equiv == 0 ) 
-				{
-					valEquiv ++ ; 
-				}
-				else if (equiv == 2 )
-				{
-					
-				}
-			}
-			
-		}
-		
-	}
-	*/
+	
 };
 
+class Conjunto
+{
+	public:
+	vector<Axioma> conjunto;
+	
+	bool ingresar(string nom, vector<Tipo> val)
+	{
+		Axioma ax(nom,val);
+		
+		vector<Axioma>::iterator it;// = this->valores.begin();
+		
+		bool ret = true;
+		
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+		{
+  		  	// en caso de que tengan el mismo nombre, pero diferente cantidad de argumentos, no se agregara a la lista
+        	if( it->nombre == ax.nombre && it->ndatos != ax.ndatos)
+        	{
+        		ret = false;
+			}
+        		
+		}
+		
+		if(ret)
+		{
+			conjunto.push_back(ax);
+		}
+		
+		return ret;
+	}
+	
+	void imprimir()
+	{
+		vector<Axioma>::iterator it;// = this->valores.begin();
+		
+		
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+		{
+  		  	it->imprimir();        		
+		}
+	}
+	
+};
 
 // axioma(tipo,tipo,tipo)
 int main()
@@ -117,16 +115,29 @@ int main()
 	
 	cout<< "hola"<<endl;
 	//string val[] = {"marco","roma"};
-	vector<string> val;
+	vector<Tipo> val;
 	
-	val.push_back("marco");
-	val.push_back("roma");
+	val.push_back(Tipo("marco"));
+	val.push_back(Tipo("roma"));
 //	cout<< sizeof(val)/sizeof(*val)<<endl<<endl;
-	string nom = "conquistador";
+	//string nom = "conquistador";
 	
-	Axioma a1(nom ,val);
 	
-	a1.imprimir();
+	Conjunto conj;
+	
+	conj.ingresar("conquistador" ,val);
+	
+	val.clear();
+	
+	val.push_back(Tipo("mer"));
+	val.push_back(Tipo("meree"));
+	conj.ingresar("conquistador",val);
+	
+	
+	
+	conj.imprimir();
+	
+	
 	
 	return 0;
 }
