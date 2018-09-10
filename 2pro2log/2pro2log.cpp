ba086b4bcb5754a007fbbd73263c9cec94a2905d
Daniel Hamilton Smith <danihas@live.com>
2018-09-09 22:41:48 -0500

    falta en conjunto hacer la funcion recursiva para igualar a todas las posibilidades, axioma y tipo ya estan funcionando. Tambien faltaria ademas una funcion que califique si un axioma es verdadero

--- 2pro2log/2pro2log.cpp
+++ 2pro2log/2pro2log.cpp
@@ -6,28 +6,81 @@
 
 using namespace std;
 
+// compara 2 char*
+bool comparar(char* cc1, char* cc2)
+{
+	int tam = strlen(cc1);
+	
+	int cant = 0 ;
+	
+	for(int a = 0 ; a < tam ; a ++)
+	{
+		
+		if(cc1[a] == cc2[a])
+		{
+			cant++;
+		}
+	}
+	
+	if( cant == tam)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
 
 class Tipo{
 	
 	public:
 		
-	char * nombre; // solo apunta en caso de comenzar con mayuscula nombre
+	char * nombre;
+	bool esVar; // guarda en caso de ser variable
 	
 //	inicializador
 	Tipo(string nom)
 	{
 		this->nombre = new char[nom.length()+1];
 		strcpy(this->nombre , nom.c_str());
+		
+		if (nombre[0] > 64 && nombre[0] < 91)
+		{
+			esVar = true;
+		}
+		else
+		{
+			esVar = false;
+		}
 	}
 	
 //	copia un valor de otro Tipo, solo en caso de este ser una variable (mayuscula)
 	void copiar(Tipo t)
 	{
-		if(nombre[0] > 64 && nombre[0] < 91)
+		if(esVar && !t.esVar)
 		{
 			nombre = t.nombre;
 		}
 	}
+	
+//	compara dos nombres
+	/*bool comparar(Tipo tt)
+	{
+//		cout<<"-------"<<endl;
+		int tam = strlen(this->nombre);
+		for(int a = 0 ; a < tam ; a ++)
+		{
+			cout <<this->nombre[a] <<"  "<< tt.nombre[a]<<endl;
+			if(this->nombre[a] != tt.nombre[a])
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}*/
 };
 
 
@@ -35,10 +88,10 @@ class Axioma{
 	
 	//private:
 	public:
+		
 	string nombre;
 	int ndatos;
 	vector<Tipo> valores; 
-	
 	// Ej: del axioma conquistador(marco,roma)  nombre = conquistador  ,  valores = [marco,roma] con ndatos = 2 (el tamaño de la lista valores)
 	
 	// se terminaria creando como : 
@@ -55,10 +108,7 @@ class Axioma{
 		this->ndatos  = val.size();
 		
 		this->valores = val;
-		
-		/*
-        */
-		
+				
 	}
 	
 //	imprime el axioma
@@ -70,11 +120,50 @@ class Axioma{
 		
 		for(it = this->valores.begin(); it != this->valores.end(); it++ )    
 		{
-  		  
-        	cout<<  it->nombre << " ";  // prints d.
-        		
+        	cout<<  it->nombre << " "; 	
 		}
 		cout<<endl;
+	}
+	
+	
+//	compara los dos axiomas y en caso que uno de estos tenga variabes, = > copia los datos de dato a Variable
+	void rellenarDatos(Axioma ax2 )
+	{
+		vector<Tipo>::iterator it;
+		vector<Tipo>::iterator it2;
+		
+		
+		it = this->valores.begin();
+		it2 = ax2.valores.begin();
+		
+		bool vabien = true;
+		
+		// verifica que todos los datos que no son variables sean iguales
+		for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ ) 
+		{
+			
+			if ( !  ( (it->esVar)  || (it2->esVar) ) &&  !comparar( it->nombre , it2->nombre )  )  
+			{
+				vabien = false;	
+				it == this->valores.end();		
+			}
+		}
+		
+		
+		
+		it = this->valores.begin();
+		it2 = ax2.valores.begin();
+		
+		
+		
+		if(vabien)
+		{
+			for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ )   
+			{
+				it->copiar(*it2) ;
+			}
+		}
+		
 	}
 	
 };
@@ -126,6 +215,34 @@ class Conjunto
 		}
 	}
 	
+	
+//	esta es la funcion que debera ser con backtracking, por ahora esta con fuerza bruta, para probar funcionaiento de 
+//	otras funciones
+	void rellenarDatos()
+	{
+		vector<Axioma>::iterator it;// = this->valores.begin();
+		
+		
+		vector<Axioma>::iterator it2;// = this->valores.begin();
+		
+		
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+		{
+//  		  	it->imprimir(); 
+			for(it2 = this->conjunto.begin(); it2 != this->conjunto.end(); it2++ )    
+			{
+//  			  	it2->imprimir();     
+					
+				if(it->nombre == it2->nombre)
+				{
+					it->rellenarDatos(*it2);
+				}   		
+			}       		
+		}
+		
+	}
+	
+	
 };
 
 // axioma(tipo,tipo,tipo)
@@ -136,29 +253,39 @@ int main()
 	//string val[] = {"marco","roma"};
 	vector<Tipo> val;
 	
-	val.push_back(Tipo("marco"));
-	val.push_back(Tipo("roma"));
+	
 //	cout<< sizeof(val)/sizeof(*val)<<endl<<endl;
 	//string nom = "conquistador";
 	
 	
 	Conjunto conj;
 	
+	val.push_back(Tipo("marco"));
+	val.push_back(Tipo("roma"));
+	val.push_back(Tipo("hola"));
 	conj.ingresar("conquistador" ,val);
 	
 	val.clear();
-	
-	val.push_back(Tipo("mer"));
-	val.push_back(Tipo("meree"));
-	
-	
-	
+	val.push_back(Tipo("marco"));
+	val.push_back(Tipo("M"));
+	val.push_back(Tipo("hola"));
 	conj.ingresar("conquistador",val);
 	
 	
+	val.clear();
+	val.push_back(Tipo("mao"));
+	val.push_back(Tipo("M"));
+	conj.ingresar("conq",val);
 	
 	conj.imprimir();
 	
+	
+	conj.rellenarDatos();
+	
+	
+	
+	cout<<endl<<endl<<"--------------------------------------------------------"<<endl<<endl;
+	conj.imprimir();
 	
 	
 	return 0;
