95802100d4cd52f2d85d350beab155a9fdbcf808
Daniel Hamilton Smith <danihas@live.com>
2018-09-11 01:05:04 -0500

    clase agregada, hay un problema que el tamaño de cada axioma no se reconoce entre predicados, pero puse que al rellenarDatos tambien compare por tamaño para evitar problemas

--- 2pro2log/2pro2log.cpp
+++ 2pro2log/2pro2log.cpp
@@ -117,11 +117,9 @@ class Axioma{
 
 		for(it = this->valores.begin(); it != this->valores.end(); it++ )
 		{
-        	cout<<  it->nombre << " ";
+        	cout<<  it->nombre << ", ";
 		}
-		cout<<endl;
 	}
-
 
 //	compara los dos axiomas y en caso que uno de estos tenga variabes, = > copia los datos de dato a Variable
 	void rellenarDatos(Axioma ax2 )
@@ -165,14 +163,15 @@ class Axioma{
 
 };
 
-class Conjunto
+class Predicado
 {
 	public:
-	vector<Axioma> conjunto;
+	vector<Axioma> axiomas;
 
 //	ingresa un axioma nuevo solo en caso de que o sea con un nombre nuevo
 //  o tenga la misma cantidad de argumentos que el axioma con el mismo nombre
 //  retorna verdadero si logro ingresar el axioma
+
 	bool ingresar(string nom, vector<Tipo> val)
 	{
 		Axioma ax(nom,val);
@@ -181,7 +180,7 @@ class Conjunto
 
 		bool ret = true;
 
-		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )
+		for(it = this->axiomas.begin(); it != this->axiomas.end(); it++ )
 		{
   		  	// en caso de que tengan el mismo nombre, pero diferente cantidad de argumentos, no se agregara a la lista
         	if( it->nombre == ax.nombre && it->ndatos != ax.ndatos)
@@ -193,28 +192,53 @@ class Conjunto
 
 		if(ret)
 		{
-			conjunto.push_back(ax);
+			axiomas.push_back(ax);
 		}
 
 		return ret;
 	}
 
-
-//	imprime el conjunto de axiomas
+//	imprime los axiomas del predicado
 	void imprimir()
 	{
 		vector<Axioma>::iterator it;// = this->valores.begin();
 
 
-		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )
+		for(it = this->axiomas.begin(); it != this->axiomas.end(); it++ )
 		{
   		  	it->imprimir();
+			cout<<" | ";
 		}
+		
 	}
 
-
-//	esta es la funcion que debera ser con backtracking, por ahora esta con fuerza bruta, para probar funcionaiento de
+//	esta es la funcion que debera ser con recursividad, por ahora esta con fuerza bruta, para probar funcionaiento de
 //	otras funciones
+	void rellenarDatos(Predicado cc2)
+	{
+		vector<Axioma>::iterator it;// = this->valores.begin();
+
+
+		vector<Axioma>::iterator it2;// = this->valores.begin();
+
+
+		for(it = this->axiomas.begin(); it != this->axiomas.end(); it++ )
+		{
+//  		  	it->imprimir();
+			for(it2 = cc2.axiomas.begin(); it2 != cc2.axiomas.end(); it2++ )
+			{
+//  			  	it2->imprimir();
+
+				if(it->nombre == it2->nombre && it->ndatos == it2->ndatos)
+				{
+					it->rellenarDatos(*it2);
+				}
+			}
+		}
+
+	}
+	/*
+//	funcion recursiva
 	void rellenarDatos()
 	{
 		vector<Axioma>::iterator it;// = this->valores.begin();
@@ -238,24 +262,63 @@ class Conjunto
 		}
 
 	}
-
+*/
 
 };
+
+
+class Conjunto{
+	
+	vector<Predicado> predicados;
+	
+	public:
+	
+	Conjunto()
+	{
+	}
+	
+	void imprimir()
+	{
+		for(int a = 0 ; a < predicados.size() ; a++)
+		{
+			predicados[a].imprimir();
+			cout<<endl;
+		}
+	}
+	
+	
+	void ingresar(Predicado pred)
+	{
+		predicados.push_back(pred);
+	}
+	
+	void rellenarDatos()
+	{
+		for(int a = 0 ; a < predicados.size() ; a++)
+		{
+			for(int b = 0 ; b < predicados.size() ; b++)
+			{
+				predicados[a].rellenarDatos(predicados[b]);
+			}
+		}
+	}
+};
+
 
 // axioma(tipo,tipo,tipo)
 
 
 //void insertarAList
 
-Conjunto leerLinea(Conjunto set, string linea);
-Conjunto leerLineaSimple(Conjunto set, string linea);
+Predicado leerLinea(Predicado set, string linea);
+Predicado leerLineaSimple(Predicado set, string linea);
 
 int main()
 {
 
-	cout<< "hola"<<endl;
+	//cout<< "hola"<<endl;
 	//string val[] = {"marco","roma"};
-	vector<Tipo> val;
+	//vector<Tipo> val;
 
 
 //	cout<< sizeof(val)/sizeof(*val)<<endl<<endl;
@@ -264,7 +327,7 @@ int main()
 
 	Conjunto conj;
 
-	val.clear();
+	/*val.clear();
 	val.push_back(Tipo("marco"));
 	val.push_back(Tipo("rusia"));
 	val.push_back(Tipo("hola"));
@@ -286,30 +349,51 @@ int main()
 	val.push_back(Tipo("mao"));
 	val.push_back(Tipo("M"));
 	conj.ingresar("conq",val);
-
+*/
+	
+	int numpred = 0 ;
+	cout<<"cantidad de predicados a ingresar :";
+	cin>>numpred;
+	
+	
+	string line;
+	cout<<"ingrese algo: \n";
+	for(int a = 0 ; a < numpred ; a ++)
+	{
+		Predicado pred;
+		
+		cout<<"> ";
+		cin>>line;
+		pred = leerLinea(pred,line);
+		conj.ingresar(pred);
+	}
+	
+	
+	
+	cout<<endl<<endl<<"--------------------------------------------------------"<<endl<<endl;
+	
+	
+	
 	conj.imprimir();
-
 
 	conj.rellenarDatos();
 
 
 
 	cout<<endl<<endl<<"--------------------------------------------------------"<<endl<<endl;
-
-	string line;
-	cout<<"ingrese algo: \n >";
-	cin>>line;
-	conj = leerLinea(conj,line);
+	
+	
+	
 	conj.imprimir();
 	return 0;
 }
 
 
-Conjunto leerLineaSimple(Conjunto set, string linea){
+Predicado leerLineaSimple(Predicado set, string linea){
 
 	int a = 0, b = 0, end = linea.size();
 
-	while(linea[b] != '(' && b < end)
+	while(linea[b] != '(' && b < end )
 		b++;
 
 	string name = linea.substr(a,b);
@@ -330,7 +414,7 @@ Conjunto leerLineaSimple(Conjunto set, string linea){
 	return set;
 }
 
-Conjunto leerLinea(Conjunto set, string linea){
+Predicado leerLinea(Predicado set, string linea){
 
 	int a = 0, b = 0, end = linea.size();
 
@@ -339,7 +423,7 @@ Conjunto leerLinea(Conjunto set, string linea){
 		while(linea[b] != ')' && b < end)
 			b++;
 
-		cout<<linea.substr(a,b+1)<<endl;
+		//cout<<linea.substr(a,b+1)<<endl;
 		if(linea[b] == ')'){
 			b++;
 			set = leerLineaSimple(set,linea.substr(a,b));
