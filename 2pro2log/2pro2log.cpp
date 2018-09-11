83515bc461ca6648af1eb809856bc1601c967e7c
Dalgomz <33590258+Dalgomz@users.noreply.github.com>
2018-09-10 20:46:04 -0500

    Añadido lectorSimple y lector compuesto por comas

--- 2pro2log/2pro2log.cpp
+++ 2pro2log/2pro2log.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -10,18 +9,17 @@ using namespace std;
 bool comparar(char* cc1, char* cc2)
 {
 	int tam = strlen(cc1);
-	
 	int cant = 0 ;
-	
+
 	for(int a = 0 ; a < tam ; a ++)
 	{
-		
+
 		if(cc1[a] == cc2[a])
 		{
 			cant++;
 		}
 	}
-	
+
 	if( cant == tam)
 	{
 		return true;
@@ -30,22 +28,22 @@ bool comparar(char* cc1, char* cc2)
 	{
 		return false;
 	}
-	
+
 }
 
 class Tipo{
-	
+
 	public:
-		
+
 	char * nombre;
 	bool esVar; // guarda en caso de ser variable
-	
+
 //	inicializador
 	Tipo(string nom)
 	{
 		this->nombre = new char[nom.length()+1];
 		strcpy(this->nombre , nom.c_str());
-		
+
 		if (nombre[0] > 64 && nombre[0] < 91)
 		{
 			esVar = true;
@@ -55,7 +53,7 @@ class Tipo{
 			esVar = false;
 		}
 	}
-	
+
 //	copia un valor de otro Tipo, solo en caso de este ser una variable (mayuscula)
 	void copiar(Tipo t)
 	{
@@ -64,7 +62,7 @@ class Tipo{
 			nombre = t.nombre;
 		}
 	}
-	
+
 //	compara dos nombres
 	/*bool comparar(Tipo tt)
 	{
@@ -78,170 +76,170 @@ class Tipo{
 				return false;
 			}
 		}
-		
+
 		return true;
 	}*/
 };
 
 class Axioma{
-	
+
 	//private:
 	public:
-		
+
 	string nombre;
 	int ndatos;
-	vector<Tipo> valores; 
+	vector<Tipo> valores;
 	// Ej: del axioma conquistador(marco,roma)  nombre = conquistador  ,  valores = [marco,roma] con ndatos = 2 (el tamaño de la lista valores)
-	
-	// se terminaria creando como : 
+
+	// se terminaria creando como :
 	// axioma(conquistador, [marco, roma] )
-	
-	
+
+
 	public:
-	
+
 //	inicializador
 	Axioma( string nom, vector<Tipo> val)
 	{
 		this->nombre  = nom;
-				
+
 		this->ndatos  = val.size();
-		
+
 		this->valores = val;
-				
+
 	}
-	
+
 //	imprime el axioma
 	void imprimir()
 	{
-		
+
 		cout<<this->nombre<<" : ";
 		vector<Tipo>::iterator it;// = this->valores.begin();
-		
-		for(it = this->valores.begin(); it != this->valores.end(); it++ )    
+
+		for(it = this->valores.begin(); it != this->valores.end(); it++ )
 		{
-        	cout<<  it->nombre << " "; 	
+        	cout<<  it->nombre << " ";
 		}
 		cout<<endl;
 	}
-	
-	
+
+
 //	compara los dos axiomas y en caso que uno de estos tenga variabes, = > copia los datos de dato a Variable
 	void rellenarDatos(Axioma ax2 )
 	{
 		vector<Tipo>::iterator it;
 		vector<Tipo>::iterator it2;
-		
-		
+
+
 		it = this->valores.begin();
 		it2 = ax2.valores.begin();
-		
+
 		bool vabien = true;
-		
+
 		// verifica que todos los datos que no son variables sean iguales
-		for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ ) 
+		for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ )
 		{
-			
-			if ( !  ( (it->esVar)  || (it2->esVar) ) &&  !comparar( it->nombre , it2->nombre )  )  
+
+			if ( !  ( (it->esVar)  || (it2->esVar) ) &&  !comparar( it->nombre , it2->nombre )  )
 			{
-				vabien = false;	
-				it == this->valores.end();		
+				vabien = false;
+				it == this->valores.end();
 			}
 		}
-		
-		
-		
+
+
+
 		it = this->valores.begin();
 		it2 = ax2.valores.begin();
-		
-		
-		
+
+
+
 		if(vabien)
 		{
-			for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ )   
+			for( it,it2 ; it2 != ax2.valores.end() ,it != this->valores.end(); it++ ,it2++ )
 			{
 				it->copiar(*it2) ;
 			}
 		}
-		
+
 	}
-	
+
 };
 
 class Conjunto
 {
 	public:
 	vector<Axioma> conjunto;
-	
+
 //	ingresa un axioma nuevo solo en caso de que o sea con un nombre nuevo
 //  o tenga la misma cantidad de argumentos que el axioma con el mismo nombre
 //  retorna verdadero si logro ingresar el axioma
 	bool ingresar(string nom, vector<Tipo> val)
 	{
 		Axioma ax(nom,val);
-		
+
 		vector<Axioma>::iterator it;// = this->valores.begin();
-		
+
 		bool ret = true;
-		
-		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )
 		{
   		  	// en caso de que tengan el mismo nombre, pero diferente cantidad de argumentos, no se agregara a la lista
         	if( it->nombre == ax.nombre && it->ndatos != ax.ndatos)
         	{
         		ret = false;
 			}
-        		
+
 		}
-		
+
 		if(ret)
 		{
 			conjunto.push_back(ax);
 		}
-		
+
 		return ret;
 	}
-	
-	
+
+
 //	imprime el conjunto de axiomas
 	void imprimir()
 	{
 		vector<Axioma>::iterator it;// = this->valores.begin();
-		
-		
-		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+
+
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )
 		{
-  		  	it->imprimir();        		
+  		  	it->imprimir();
 		}
 	}
-	
-	
-//	esta es la funcion que debera ser con backtracking, por ahora esta con fuerza bruta, para probar funcionaiento de 
+
+
+//	esta es la funcion que debera ser con backtracking, por ahora esta con fuerza bruta, para probar funcionaiento de
 //	otras funciones
 	void rellenarDatos()
 	{
 		vector<Axioma>::iterator it;// = this->valores.begin();
-		
-		
+
+
 		vector<Axioma>::iterator it2;// = this->valores.begin();
-		
-		
-		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )    
+
+
+		for(it = this->conjunto.begin(); it != this->conjunto.end(); it++ )
 		{
-//  		  	it->imprimir(); 
-			for(it2 = this->conjunto.begin(); it2 != this->conjunto.end(); it2++ )    
+//  		  	it->imprimir();
+			for(it2 = this->conjunto.begin(); it2 != this->conjunto.end(); it2++ )
 			{
-//  			  	it2->imprimir();     
-					
+//  			  	it2->imprimir();
+
 				if(it->nombre == it2->nombre)
 				{
 					it->rellenarDatos(*it2);
-				}   		
-			}       		
+				}
+			}
 		}
-		
+
 	}
-	
-	
+
+
 };
 
 // axioma(tipo,tipo,tipo)
@@ -249,63 +247,65 @@ class Conjunto
 
 //void insertarAList
 
-Conjunto leerLineaSimple(Conjunto set);
+Conjunto leerLinea(Conjunto set, string linea);
+Conjunto leerLineaSimple(Conjunto set, string linea);
 
 int main()
 {
-	
+
 	cout<< "hola"<<endl;
 	//string val[] = {"marco","roma"};
 	vector<Tipo> val;
-	
-	
+
+
 //	cout<< sizeof(val)/sizeof(*val)<<endl<<endl;
 	//string nom = "conquistador";
-	
-	
+
+
 	Conjunto conj;
-	
+
 	val.clear();
 	val.push_back(Tipo("marco"));
 	val.push_back(Tipo("rusia"));
 	val.push_back(Tipo("hola"));
 	conj.ingresar("conquistador" ,val);
-	
+
 	val.clear();
 	val.push_back(Tipo("marco"));
 	val.push_back(Tipo("M"));
 	val.push_back(Tipo("hola"));
 	conj.ingresar("conquistador",val);
-	
+
 	val.clear();
 	val.push_back(Tipo("marco"));
 	val.push_back(Tipo("roma"));
 	val.push_back(Tipo("hola"));
 	conj.ingresar("conquistador" ,val);
-	
+
 	val.clear();
 	val.push_back(Tipo("mao"));
 	val.push_back(Tipo("M"));
 	conj.ingresar("conq",val);
-	
+
 	conj.imprimir();
-	
-	
+
+
 	conj.rellenarDatos();
-	
-	
-	
+
+
+
 	cout<<endl<<endl<<"--------------------------------------------------------"<<endl<<endl;
+
+	string line;
+	cout<<"ingrese algo: \n >";
+	cin>>line;
+	conj = leerLinea(conj,line);
 	conj.imprimir();
-	
-	
 	return 0;
 }
 
-Conjunto leerLineaSimple(Conjunto set){
-	string linea;
-	cout<<"Ingrese algo: \n >";
-	cin>>linea;
+
+Conjunto leerLineaSimple(Conjunto set, string linea){
 
 	int a = 0, b = 0, end = linea.size();
 
@@ -327,5 +327,25 @@ Conjunto leerLineaSimple(Conjunto set){
 		b++;
 	}
 	set.ingresar(name,fields);
+	return set;
+}
+
+Conjunto leerLinea(Conjunto set, string linea){
+
+	int a = 0, b = 0, end = linea.size();
+
+	while(a+b < end){
+		a = b;
+		while(linea[b] != ')' && b < end)
+			b++;
+
+		if(linea[b] == ')'){
+			b++;
+			cout<<linea.substr(a,b)<<endl;
+			set = leerLineaSimple(set,linea.substr(a,b));
+			b++;
+		}
+	}
+
 	return set;
 }
