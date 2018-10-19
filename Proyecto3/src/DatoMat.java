23540fa5e52f30e927792a47da593b3fd51b4b21
dhas <danihas@live.com>
2018-10-19 17:34:36 -0500

    cambio bobo para que sean mas legibles los datos en la matriz

--- Proyecto3/src/DatoMat.java
+++ Proyecto3/src/DatoMat.java
@@ -67,13 +67,13 @@ public class DatoMat {
 
    public String toString() 
    {
-      String ret = "";
+      String ret = "| ";
       for (int a = 0 ; a < categorias.size() ; a ++ )
       {
-         ret += categorias.get(a) + " ";
+         ret += categorias.get(a) + " | ";
       }
       
-      return  ret +": "+ porcentaje;
+      return  ret +": "+ porcentaje + "\n";
       //return getClass().getName() + "@" + Integer.toHexString(hashCode());
    }
 }
